6e58e207efa3534cb5f4ec1a699d7b08be91b124
Diego Broetto <deigo.broetto@darede.com.br>
2024-09-14 15:31:22 -0300

    fix: fixing the outputs

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,15 +1,15 @@
 output "subnet_pub_1a" {
-  value = aws_subnet.eks_subnet_pub_1a.id
+  value = aws_subnet.eks_subnet_pub_1a
 }
 
 output "subnet_pub_1b" {
-  value = aws_subnet.eks_subnet_pub_1b.id
+  value = aws_subnet.eks_subnet_pub_1b
 }
 
 output "subnet_priv_1a" {
-  value = aws_subnet.eks_subnet_priv_1a.id
+  value = aws_subnet.eks_subnet_priv_1a
 }
 
 output "subnet_priv_1b" {
-  value = aws_subnet.eks_subnet_priv_1b.id
+  value = aws_subnet.eks_subnet_priv_1b
 }
---
+++ output.tf
@@ -0,0 +1,15 @@
+output "subnet_pub_1a" {
+  value = module.eks_network.subnet_pub_1a.id
+}
+
+output "subnet_pub_1b" {
+  value = module.eks_network.subnet_pub_1b.id
+}
+
+output "subnet_priv_1a" {
+  value = module.eks_network.subnet_priv_1a.id
+}
+
+output "subnet_priv_1b" {
+  value = module.eks_network.subnet_priv_1b.id
+}

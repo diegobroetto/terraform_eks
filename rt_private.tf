80817e8d6538891d380d455cf5a6609dd094f3f0
Diego Broetto <deigo.broetto@darede.com.br>
2024-09-14 14:51:53 -0300

    feat: creating route tables for private subnets

---
+++ rt_private.tf
@@ -0,0 +1,34 @@
+resource "aws_route_table" "rt_private-1a" {
+  vpc_id = aws_vpc.vpc_eks.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw_1a.id
+  }
+
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-private-rt-1a"
+    }
+  )
+}
+
+resource "aws_route_table" "rt_private-1b" {
+  vpc_id = aws_vpc.vpc_eks.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ngw_1b.id
+  }
+
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-private-rt-1b"
+    }
+  )
+}
+

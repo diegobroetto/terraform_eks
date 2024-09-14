3df37d6ad97714c0599faf97ae59736fb8ca68cd
Diego Broetto <deigo.broetto@darede.com.br>
2024-09-14 14:21:12 -0300

    feat: create public route table

---
+++ rt_public.tf
@@ -0,0 +1,16 @@
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc_eks.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-public-rt"
+    }
+  )
+}

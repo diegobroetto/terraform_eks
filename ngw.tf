45b8fcb3e8d1b0b0a0433c41ece7f5d46ecf5b68
Diego Broetto <deigo.broetto@darede.com.br>
2024-09-14 14:44:18 -0300

    feat: creating ngw

---
+++ ngw.tf
@@ -0,0 +1,47 @@
+resource "aws_eip" "ngw_eip_1a" {
+  domain = "standard"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-eip-1a"
+    }
+  )
+}
+
+resource "aws_eip" "ngw_eip_1b" {
+  domain = "standard"
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-eip-1b"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "ngw_1a" {
+  allocation_id = aws_eip.ngw_eip_1a.id
+  subnet_id     = aws_subnet.eks_subnet_pub_1a.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1a"
+    }
+  )
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "ngw_1b" {
+  allocation_id = aws_eip.ngw_eip_1b.id
+  subnet_id     = aws_subnet.eks_subnet_pub_1b.id
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "${var.project_name}-ngw-1b"
+    }
+  )
+
+  depends_on = [aws_internet_gateway.igw]
+}

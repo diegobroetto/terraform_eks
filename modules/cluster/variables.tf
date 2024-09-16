dcd6fa8f509424565b2cbb171703d784305af165
Diego Broetto <deigo.broetto@darede.com.br>
2024-09-16 11:08:36 -0300

    feat: the cluster module is born

---
+++ modules/cluster/variables.tf
@@ -0,0 +1,20 @@
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name de resources (Name Tag)"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Default tags to be used on the project"
+}
+
+variable "public_subnet_1a" {
+  type        = string
+  description = "Public Subnet to create a EKS Cluster AZ 1a"
+}
+
+variable "public_subnet_1b" {
+  type        = string
+  description = "Public Subnet to create a EKS Cluster AZ 1b"
+}
